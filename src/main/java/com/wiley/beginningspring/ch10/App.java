07a9659fe7f1dc0da954e905c8730ee78ae4c317
frankguekeng <frankguekeng11@gmail.com>
2025-05-13 21:39:41 +0100

    Cache Manager with an Anotation

--- src/main/java/com/wiley/beginningspring/ch10/App.java
+++ src/main/java/com/wiley/beginningspring/ch10/App.java
@@ -1,8 +1,10 @@
 package com.wiley.beginningspring.ch10;
 
+import com.wiley.beginningspring.ch10.config.ApplicationConfig;
 import com.wiley.beginningspring.ch10.model.User;
 import com.wiley.beginningspring.ch10.service.UserService;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
@@ -13,12 +15,15 @@ public class App
 {
     public static void main( String[] args )
     {
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(ApplicationConfig.class);
         UserService userService = applicationContext.getBean(UserService.class);
         User userFetch1 = userService.getUser(1);
         System.out.println(userFetch1);
 
-        User userFetch2 = userService.getUser(1);
+        User userFetch2 = userService.getUser(userFetch1);
         System.out.println(userFetch2);
+
+        User userFetch3 = userService.getUser(userFetch1);
+        System.out.println(userFetch3);
     }
 }
--- src/main/java/com/wiley/beginningspring/ch10/service/UserService.java
+++ src/main/java/com/wiley/beginningspring/ch10/service/UserService.java
@@ -2,10 +2,13 @@ package com.wiley.beginningspring.ch10.service;
 
 import com.wiley.beginningspring.ch10.model.User;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Component
 public class UserService {
 
     private Map<Integer, User> users = new HashMap<>();
@@ -19,6 +22,11 @@ public class UserService {
     public User getUser(int id){
         System.out.println("User with id " + id + " requested");
         return users.get(id);
+    }
+    @Cacheable(value = "users", key = "#user.id")
+    public User getUser(User user){
+        System.out.println("User with id " + user.getId() + " requested");
+        return users.get(user.getId());
     }
 
 }
